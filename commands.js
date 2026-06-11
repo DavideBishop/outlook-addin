ad32faf8c9d7c6a6d3e8f5dca5f39fcc7b3c6d0a
DavideBishop <davide@bishopfraser.co.za>
2026-06-11 10:41:57 +0200

    Update commands.js

--- commands.js
+++ commands.js
@@ -1,22 +1,50 @@
-Office.onReady();
+Office.initialize = function () {};
 
 function onItemSend(event) {
-  Office.context.mailbox.item.subject.getAsync(function(result) {
-    if (result.status !== Office.AsyncResultStatus.Succeeded) {
+  var item = Office.context.mailbox.item;
+
+  item.subject.getAsync(function (subjectResult) {
+    if (subjectResult.status !== Office.AsyncResultStatus.Succeeded) {
       event.completed({ allowEvent: true });
       return;
     }
 
-    var subject = (result.value || "").trim();
-    var hasCode = /^\[.+?\]/.test(subject);
+    var subject = subjectResult.value || "";
+    var hasCode = /^\[.+?\]/.test(subject.trim());
 
     if (hasCode) {
       event.completed({ allowEvent: true });
-    } else {
-      event.completed({
-        allowEvent: false,
-        errorMessage: "Reminder: no client code detected. Add [CODE] to the start of the subject, or click Send Anyway to send without one."
-      });
+      return;
     }
+
+    item.loadCustomPropertiesAsync(function (propsResult) {
+      if (propsResult.status !== Office.AsyncResultStatus.Succeeded) {
+        event.completed({ allowEvent: true });
+        return;
+      }
+
+      var props = propsResult.value;
+      var bypass = props.get("bypassCodeReminder");
+
+      if (bypass) {
+        props.set("bypassCodeReminder", false);
+        props.saveAsync(function () {
+          event.completed({ allowEvent: true });
+        });
+        return;
+      }
+
+      props.set("bypassCodeReminder", true);
+      props.saveAsync(function () {
+        item.notificationMessages.addAsync("clientCodeReminder", {
+          type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+          message: "Reminder: no client code prefix detected. Add [CODE] to the subject, or click Send again to send without one.",
+          icon: "Icon.16x16",
+          persistent: false,
+        }, function () {
+          event.completed({ allowEvent: false });
+        });
+      });
+    });
   });
 }
